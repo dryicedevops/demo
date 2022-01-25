1f4b902d8579526499a743aad12bbbdd01619efb
dryicedevops <98248798+dryicedevops@users.noreply.github.com>
2022-01-25 16:19:44 +0530

    updated var file

--- Envprovisioning/variable.tf
+++ Envprovisioning/variable.tf
@@ -1,25 +1,30 @@
 variable namespace {
-    description = "The namespace in which the resources are deployed such as dev, qa, prod"
+    description = "The namespace in which the resources are deployed such as dev, test, nonprod, prod"
+    default = "test"
    }
 
 variable location {
     description = "The location in which the resources are deployed such as east US, west Europe"
-    
+    default = "east US"
 }
 
 variable storage_account_tier {
     description = "Stoarge Account Tier"
+    default = "Standard"
     
 }
 
 variable account_replication_type {
     description = "Stoarge Account Replication Type"
+    default = "LRS"
 }
 
 variable storage_account_name {
     description = "Name of the storage acount"
+    default = "demostaccounttest1234"
 }
 
 variable environment {
     description = "The environment in which the resources are deployed such as dev, qa, prod"
+    default = "testing"
 }
